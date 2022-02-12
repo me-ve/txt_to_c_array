1b6e601f2a86086b59a993d735f488f4e590f98d
me-ve <68286584+me-ve@users.noreply.github.com>
2022-02-12 15:10:25 +0100

    Initial commit

---
+++ txt_to_c_array.c
@@ -0,0 +1,37 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ARRAY_TYPE_INDEX 3
+#define ARRAY_NAME_INDEX 4
+
+int main(int argc, char **argv){
+	if(argc != 5){
+		fprintf(stderr, "Usage: %s <input> <output> <array_type> <array_name>\n", argv[0]);
+		return 1;
+	}
+	FILE *input = fopen(argv[1], "r");
+	if(input == NULL){
+		fprintf(stderr, "File %s doesn't exist.\n", argv[1]);
+		return 2;
+	}
+	FILE *output = fopen(argv[2], "w");
+	if(output == NULL){
+		fprintf(stderr, "File %s cannot be created.\n", argv[2]);
+		return 3;
+	}
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	fprintf(output, "%s %s[] = {\n", argv[ARRAY_TYPE_INDEX], argv[ARRAY_NAME_INDEX]);
+	while((read = getline(&line, &len, input) != -1)){
+		char *c = strchr(line, '\n');
+		if(c){
+			*c = ',';
+		}
+		fprintf(output, "\t%s\n", line);
+	}
+	fprintf(output, "};\n");
+	fclose(input);
+	fclose(output);
+}
