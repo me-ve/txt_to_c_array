c3a1aecbdb48944d005daf59ddcaab5c3e006f68
me-ve <68286584+me-ve@users.noreply.github.com>
2022-05-22 19:26:36 +0200

    Update txt_to_c_array.c

--- txt_to_c_array.c
+++ txt_to_c_array.c
@@ -2,36 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #define ARRAY_TYPE_INDEX 3
 #define ARRAY_NAME_INDEX 4
 
 int main(int argc, char **argv){
 	if(argc != 5){
 		fprintf(stderr, "Usage: %s <input> <output> <array_type> <array_name>\n", argv[0]);
-		return 1;
+		return ENOTSUP;
 	}
 	FILE *input = fopen(argv[1], "r");
 	if(input == NULL){
 		fprintf(stderr, "File %s doesn't exist.\n", argv[1]);
-		return 2;
+		return ENOENT;
 	}
 	FILE *output = fopen(argv[2], "w");
 	if(output == NULL){
 		fprintf(stderr, "File %s cannot be created.\n", argv[2]);
-		return 3;
+		return EIO;
 	}
 	char *line = NULL;
+	char *c = NULL;
 	size_t len = 0;
 	ssize_t read;
 	fprintf(output, "%s %s[] = {\n", argv[ARRAY_TYPE_INDEX], argv[ARRAY_NAME_INDEX]);
 	while((read = getline(&line, &len, input) != -1)){
-		char *c = strchr(line, '\n');
+		c = strchr(line, '\n');
 		if(c){
 			*c = ',';
 		}
 		fprintf(output, "\t%s\n", line);
 	}
+	free(line);
 	fprintf(output, "};\n");
 	fclose(input);
 	fclose(output);
+	return 0;
 }
